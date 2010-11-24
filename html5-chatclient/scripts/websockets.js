812607e484b670e254932a3fda3be4843e05640c
km <km@km-ow-laptop.(none)>
2010-11-24 22:57:03 +0100

    online users now kinda works

--- html5-chatclient/scripts/websockets.js
+++ html5-chatclient/scripts/websockets.js
@@ -7,45 +7,44 @@ $(function() {
     conn.onOpen = function() { console.log("connection established!"); }
     conn.onError = function(err) { alert("connection failed: " + err.msg); }
 
-    conn.subscribe("my_events");
+	 conn.subscribe("my_events");
 
-    conn.onSubscribed = function(channelName, _subscription) {
-        ourUsername = conn.username;
-	users.push(ourUsername);
-        updateUserList();
-        console.log("onsubscribed with username " + ourUsername);
-        subscription = _subscription;
+	 conn.onSubscribed = function(channelName, _subscription) {
+	 ourUsername = conn.username;
+	 users.push(ourUsername);
+	 updateUserList();
+	 console.log("onsubscribed with username " + ourUsername);
+	 subscription = _subscription;
 
-        subscription.onPublish = function(frame) {
-            if (frame.user != ourUsername) {
-                console.log(frame.user + " said: " + frame.payload);
-                html5team4.webdb.addPost(JSON.parse(frame.payload));
-            }
-        }
-	subscription.onSubscribed = function(frame) {
-          //alert("user: " + frame.user + " has subscribed!");
-            console.log(frame.user + " connected");
-	    users.push(frame.user);
-            updateUserList();
+	 subscription.onPublish = function(frame) {
+	 	if (frame.user != ourUsername) {
+	 		console.log(frame.user + " said: " + frame.payload);
+	 		html5team4.webdb.addPost(JSON.parse(frame.payload));
+	 	}
+	 }
 
-        }
+	 subscription.onSubscribe = function(frame) {
+				console.log(frame.user + " connected");
+				users.push(frame.user);
+				updateUserList();
+	 }
 
-      	subscription.onUnsubscribe = function(frame) {
-  	  //alert("user: " + frame.user + " has unsubscribed!");
-                console.log(frame.user + " disconnected");
-		var toRemove = -1;
-		$.each(users, function(index, user) { 
-  		   if(frame.user == user) {
-			toRemove = index;
-		   }
-		});
-		if(toRemove != -1) {
-		   users.remove(toRemove)
-		}
-		updateUserList();
-            }
+	 subscription.onUnsubscribe = function(frame) {
+				console.log(frame.user + " disconnected");
+				var toRemove = -1;
+				$.each(users, function(index, user) { 
+					if(frame.user == user) {
+						toRemove = index;
+					}
+				});
 
-    }
+				if(toRemove != -1) {
+					users.remove(toRemove)
+				}
+				updateUserList();
+	 		}
+
+	 }
 
 
 
@@ -53,14 +52,14 @@ $(function() {
 });
 
 function publishPost(post) {
-    console.log("skal publishe %o ", post);
-    console.log(subscription);
-    try {
-        conn.publish("my_events", JSON.stringify(post));
+	  console.log("skal publishe %o ", post);
+	  console.log(subscription);
+	  try {
+				 conn.publish("my_events", JSON.stringify(post));
 
-    } catch (e) {
-        console.log(e);
-    }
+	  } catch (e) {
+				 console.log(e);
+	  }
 }
 function updateUserList() {
    console.debug("update users")
